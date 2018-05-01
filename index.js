ae65d69f04429c1b8cf1b5a446fc585b74a32099
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-05-02 03:23:18 +0800

    Progress up to search results

--- index.js
+++ index.js
@@ -8,24 +8,69 @@ require('dotenv').config();
 const app = express();
 
 app.listen(5051, async () => {
-  console.log('STARTING');
+  const CREDENTIALS = {
+    username: process.env.USERID,
+    password: process.env.PASSWORD
+  };
 
-  const params = {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
+  const PARAMS = {
     status: process.env.STATUS_VALUE,
     searchPriceMin: process.env.SEARCH_PRICE_MIN,
     searchPriceMax: process.env.SEARCH_PRICE_MAX,
-    monthsBack: process.env.MONTHS_BACK,
+    monthsBack: process.env.MONTHS_BACK
   };
-  
-  console.log('params:', params);
+
+  console.log('credentials:', CREDENTIALS);
+  console.log('params:', PARAMS);
 
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 250
+    slowMo: 10
   });
   const page = await browser.newPage();
 
+  // Go to login page and authenticate
   await page.goto('https://sabor.connectmls.com/cvlogin.jsp');
+
+  const USERNAME_SELECTOR =
+    '.login-credentials .login-input input[name=userid]';
+  const PASSWORD_SELECTOR =
+    '.login-credentials .login-input input[name=password]';
+  const SIGN_IN_SELECTOR = '.login-button input[name=login]';
+
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(CREDENTIALS.username);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(CREDENTIALS.password);
+
+  await page.click(SIGN_IN_SELECTOR);
+  await page.waitFor(2 * 1000); // Workaround for now, waitForNavigation is not firing
+
+  // Go to search Tab
+  const SEARCH_SELECTOR = '#search > div';
+
+  await page.click(SEARCH_SELECTOR);
+  await page.waitFor(5 * 1000);
+
+  // Manipulate search form
+  let frame = await page.frames().find(f => f.name() === 'workspace');
+
+  async function setElementValue(sel, val) {
+    frame.evaluate(
+      data => {
+        return (document.querySelector(data.sel).value = data.val);
+      },
+      { sel, val }
+    );
+  }
+
+  await setElementValue('#STATUSID', PARAMS.status);
+  await setElementValue('#minSRCHPRICE', PARAMS.searchPriceMin);
+  await setElementValue('#maxSRCHPRICE', PARAMS.searchPriceMax);
+  await setElementValue('#MONTHS_BACKID', PARAMS.monthsBack);
+
+  // Search results
+  const VIEW_RESULTS_SELECTOR = await frame.$('#searchButtonTop');
+  await VIEW_RESULTS_SELECTOR.click();
 });
