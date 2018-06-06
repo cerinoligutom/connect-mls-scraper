046409960dec9a8ab8ae4508de16cb8cd30ab2ce
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-06-06 22:29:23 +0800

    Error handling and skip if agent has no email

--- scraper.js
+++ scraper.js
@@ -37,7 +37,107 @@ const PARAMS = {
   monthsBack: process.env.MONTHS_BACK
 };
 
-app.listen(+process.env.PORT || 5050, async () => {
+let allAgentDetails = [];
+let errorCount = 0;
+let currentListingCount = 1;
+
+let writeToFile = () => {
+  log(chalk.bold.magenta('Preparing data for writing to CSV...'));
+
+  let dateNow = new Date().getTime();
+
+  allAgentDetails = _.uniqBy(allAgentDetails, 'Name');
+
+  let csv = csvjson.toCSV(allAgentDetails, {
+    headers: 'key',
+    wrap: true
+  });
+
+  const instancePath = (fileName) => {
+    return path.join(__dirname, `output/instances${(fileName) ? `/${fileName}`: ''}`);
+  }
+
+  const checkpointsPath = (fileName) => {
+    return path.join(__dirname, `output/checkpoints${(fileName) ? `/${fileName}`: ''}`);
+  }
+
+  const outputPath = (fileName) => {
+    return path.join(__dirname, `output${(fileName) ? `/${fileName}`: ''}`);
+  }
+
+  log(chalk.bold.magenta(`Writing instance data to file @ ${instancePath(`${dateNow}.csv`)}...`));
+  fsPath.writeFileSync(instancePath(`${dateNow}.csv`), csv, 'utf8');
+  log(chalk.bold.magenta('Successfully written to instances!'));
+
+  log(chalk.bold.magenta('Checking if agents.csv exists...'));
+  if (!fs.existsSync(outputPath('agents.csv'))) {
+    log(chalk.bold.red('agents.csv NOT FOUND! Initializing...'));
+
+    fsPath.writeFileSync(checkpointsPath(`agents-${dateNow}.csv`), csv, 'utf8'); 
+    fsPath.writeFileSync(outputPath(`agents.csv`), csv, 'utf8'); 
+
+    log(chalk.bold.magenta('Done!'));
+    log('');
+
+    log(chalk.bold.magenta('---------------------- SUMMARY ----------------------'));
+    log(chalk.bold.magenta(`-> Number of NEW agents FOUND for this scrape instance: ${allAgentDetails.length}`));
+  } else {
+    log(chalk.bold.magenta('agents.csv FOUND! Preparing...'));      
+
+    let data = fs.readFileSync(outputPath('agents.csv'), { encoding: 'utf8' });
+    data = csvjson.toSchemaObject(data, { quote: true });
+
+    let dataCountBefore = data.length;
+    let agentsUpdatedCount = 0;
+
+    log(chalk.bold.magenta('Updating data...'));
+
+    allAgentDetails = _.uniqBy(allAgentDetails, 'Name');
+
+    allAgentDetails.forEach(agent => {
+      let dataAgentIndex = _.findIndex(data, dataAgent => dataAgent.Name.trim() === agent.Name.trim());
+
+      // If agent exists, update fields
+      if (dataAgentIndex > -1) {
+        agentsUpdatedCount++;
+        let dataAgent = data[dataAgentIndex];
+        for (let key in agent) {
+          // Condition to NOT REPLACE existing dataAgent if newly scraped agent has no value for it
+          if (!!agent[key].trim()) {
+            dataAgent[key] = agent[key];
+          }
+        } 
+      } else {  // Agent is new, insert
+        data.push(agent);
+      }
+    });
+
+    let dataCountAfter = data.length;
+
+    data = _.uniqBy(data, 'Name');
+
+    let updatedData = csvjson.toCSV(data, {
+      headers: 'key',
+      wrap: true
+    });
+
+    fsPath.writeFileSync(checkpointsPath(`agents-${dateNow}.csv`), updatedData, 'utf8');     
+    fsPath.writeFileSync(outputPath(`agents.csv`), updatedData, 'utf8');
+
+    log(chalk.bold.magenta('Done!'));
+    log('');
+
+    log(chalk.bold.magenta('---------------------- SUMMARY ----------------------'));
+    log(chalk.bold.magenta(`-> Number of agents BEFORE this scrape instance: ${dataCountBefore}`));
+    log(chalk.bold.magenta(`-> Number of agents AFTER this scrape instance: ${dataCountAfter}`));
+    log(chalk.bold.magenta(`-> Number of EXISTING (and possibly updated) agents for this scrape instance: ${agentsUpdatedCount}`));    
+    log(chalk.bold.magenta(`-> Number of NEW agents FOUND for this scrape instance: ${dataCountAfter - dataCountBefore}`));
+  }
+
+  log(chalk.bold.bgRed.white('Error/Skipped count:', errorCount));
+}
+
+let scrape = async () => {
   log(chalk.bold.magenta('Checking .env parameters...'));
 
   log(chalk.bold.magenta('Checking Credentials parameters...'));  
@@ -154,12 +254,17 @@ app.listen(+process.env.PORT || 5050, async () => {
     'table td:nth-child(3) b:nth-child(2)',
     element => +element.innerText
   );
-  let currentListingCount = 1;
 
-  let allAgentDetails = [];
+
+  let showError = (message) => {
+    log(chalk.bold.bgRed.white('error @:', message));
+    log(chalk.bold.bgRed.white('SKIPPING'));
+    log('');
+  }
 
   log(chalk.bold.magenta('Magic start!'));
-  while (currentListingCount <= totalListings) {    
+  while (currentListingCount <= totalListings) {
+    let hasFailed = false;
     if (process.env.SEARCH_RESULTS_LIMIT) {
       log(chalk.bold.bgRed.white(`SEARCH_RESULTS_LIMIT Parameter is set! Stopping @ ${process.env.SEARCH_RESULTS_LIMIT}`));
       if (currentListingCount > +process.env.SEARCH_RESULTS_LIMIT) break;
@@ -179,12 +284,51 @@ app.listen(+process.env.PORT || 5050, async () => {
 
     let nextButton = await navpanelFrame.$(
       'table td:nth-child(4) > div.nextBtn'
-    );
+    ).catch(() => {
+      showError('Retrieving nextButton');
+      hasFailed = true;
+      return;
+    });
+    if (hasFailed || !nextButton) {
+      showError('nextButton empty');
+      hasFailed = true;
+      errorCount++;
+      currentListingCount++;      
+      await navpanelFrame.waitFor(+process.env.LISTING_PAGE_DELAY || 1000);
+      continue;
+    }
+
 
     let agentTable = await workspaceFrame.$(
       'div#listingspane div.report table:nth-child(8)'
-    );
-    let agentLink = await agentTable.$('tr:nth-child(2) td:nth-child(2) a');
+    ).catch(() => {
+      showError('Retrieving agentTable');
+      hasFailed = true;
+      return;
+    });
+    if (hasFailed || !agentTable) {
+      showError('agentTable empty');
+      hasFailed = true;
+      errorCount++;      
+      currentListingCount++;      
+      await navpanelFrame.waitFor(+process.env.LISTING_PAGE_DELAY || 1000);
+      continue;
+    }
+
+    let agentLink = await agentTable.$('tr:nth-child(2) td:nth-child(2) a').catch(() => {
+      showError('Retrieving agentLink');
+      hasFailed = true;
+      return;
+    });
+    if (hasFailed || !agentLink) {
+      showError('agentLink empty');
+      hasFailed = true;
+      errorCount++;
+      currentListingCount++;      
+      await navpanelFrame.waitFor(+process.env.LISTING_PAGE_DELAY || 1000);
+      continue;
+    }
+
     let href = await agentLink.getProperty('href');
     let value = await href.jsonValue();
     value = value.split(`'`)[1];
@@ -199,13 +343,41 @@ app.listen(+process.env.PORT || 5050, async () => {
     let agent = await currentAgentPage.$eval(
       'table table table table tr strong',
       element => element.innerText
-    );
+    ).catch(() => {
+      showError('Retrieving agent');
+      hasFailed = true;
+      return;
+    });
+    if (hasFailed || !agent) {
+      showError('agent empty');
+      hasFailed = true;
+      errorCount++;
+      currentListingCount++;
+      await currentAgentPage.close();
+      await navpanelFrame.waitFor(+process.env.LISTING_PAGE_DELAY || 1000);
+      continue;
+    }
+
     log(chalk.bold.green(`Agent Name: ${agent}`));
 
     let details = await currentAgentPage.$eval(
       'table table table table tr:nth-child(2) td:last-child',
       element => element.innerText.split('\n')
-    );
+    ).catch(() => {
+      showError('Retrieving details');
+      hasFailed = true;
+      return;
+    });
+    if (hasFailed || !details) {
+      showError('details empty');
+      hasFailed = true;
+      errorCount++;     
+      currentListingCount++; 
+      await currentAgentPage.close();
+      await navpanelFrame.waitFor(+process.env.LISTING_PAGE_DELAY || 1000);
+      continue;
+    }
+
     log(chalk.bold.green(`Raw details scraped:`));
     log(chalk.bold.green(JSON.stringify(details, null, 2)));
 
@@ -247,104 +419,34 @@ app.listen(+process.env.PORT || 5050, async () => {
     log(chalk.bold.blue(JSON.stringify(agentDetails, null, 2)));
     log('');
 
+    currentListingCount++;
+
+    if (agentDetails.Email.trim().length === 0) {
+      log(chalk.bold.blue('SKIPPING. REASON: Agent doesn\'t have email'));
+      continue;
+    }
+
     allAgentDetails.push(agentDetails);
     await currentAgentPage.close();
-    currentListingCount++;
   }
 
   await browser.close();
 
-  log(chalk.bold.magenta('Preparing data for writing to CSV...'));
+  writeToFile();
 
-  let dateNow = new Date().getTime();
+  process.exit(0);
+};
 
-  allAgentDetails = _.uniqBy(allAgentDetails, 'Name');
-
-  let csv = csvjson.toCSV(allAgentDetails, {
-    headers: 'key',
-    wrap: true
-  });
-
-  const instancePath = (fileName) => {
-    return path.join(__dirname, `output/instances${(fileName) ? `/${fileName}`: ''}`);
-  }
-
-  const checkpointsPath = (fileName) => {
-    return path.join(__dirname, `output/checkpoints${(fileName) ? `/${fileName}`: ''}`);
-  }
-
-  const outputPath = (fileName) => {
-    return path.join(__dirname, `output${(fileName) ? `/${fileName}`: ''}`);
-  }
-
-  log(chalk.bold.magenta(`Writing instance data to file @ ${instancePath(`${dateNow}.csv`)}...`));
-  fsPath.writeFileSync(instancePath(`${dateNow}.csv`), csv, 'utf8');
-  log(chalk.bold.magenta('Successfully written to instances!'));
-
-  log(chalk.bold.magenta('Checking if agents.csv exists...'));
-  if (!fs.existsSync(outputPath('agents.csv'))) {
-    log(chalk.bold.red('agents.csv NOT FOUND! Initializing...'));
-
-    fsPath.writeFileSync(checkpointsPath(`agents-${dateNow}.csv`), csv, 'utf8'); 
-    fsPath.writeFileSync(outputPath(`agents.csv`), csv, 'utf8'); 
-
-    log(chalk.bold.magenta('Done!'));
-    log('');
-
-    log(chalk.bold.magenta('---------------------- SUMMARY ----------------------'));
-    log(chalk.bold.magenta(`-> Number of NEW agents FOUND for this scrape instance: ${allAgentDetails.length}`));
-  } else {
-    log(chalk.bold.magenta('agents.csv FOUND! Preparing...'));      
-
-    let data = fs.readFileSync(outputPath('agents.csv'), { encoding: 'utf8' });
-    data = csvjson.toSchemaObject(data, { quote: true });
-
-    let dataCountBefore = data.length;
-    let agentsUpdatedCount = 0;
-
-    log(chalk.bold.magenta('Updating data...'));
-
-    allAgentDetails = _.uniqBy(allAgentDetails, 'Name');
-
-    allAgentDetails.forEach(agent => {
-      let dataAgentIndex = _.findIndex(data, dataAgent => dataAgent.Name.trim() === agent.Name.trim());
-
-      // If agent exists, update fields
-      if (dataAgentIndex > -1) {
-        agentsUpdatedCount++;
-        let dataAgent = data[dataAgentIndex];
-        for (let key in agent) {
-          // Condition to NOT REPLACE existing dataAgent if newly scraped agent has no value for it
-          if (!!agent[key].trim()) {
-            dataAgent[key] = agent[key];
-          }
-        } 
-      } else {  // Agent is new, insert
-        data.push(agent);
-      }
-    });
-
-    let dataCountAfter = data.length;
-
-    data = _.uniqBy(data, 'Name');
-
-    let updatedData = csvjson.toCSV(data, {
-      headers: 'key',
-      wrap: true
-    });
-
-    fsPath.writeFileSync(checkpointsPath(`agents-${dateNow}.csv`), updatedData, 'utf8');     
-    fsPath.writeFileSync(outputPath(`agents.csv`), updatedData, 'utf8');
-
-    log(chalk.bold.magenta('Done!'));
-    log('');
-
-    log(chalk.bold.magenta('---------------------- SUMMARY ----------------------'));
-    log(chalk.bold.magenta(`-> Number of agents BEFORE this scrape instance: ${dataCountBefore}`));
-    log(chalk.bold.magenta(`-> Number of agents AFTER this scrape instance: ${dataCountAfter}`));
-    log(chalk.bold.magenta(`-> Number of EXISTING (and possibly updated) agents for this scrape instance: ${agentsUpdatedCount}`));    
-    log(chalk.bold.magenta(`-> Number of NEW agents FOUND for this scrape instance: ${dataCountAfter - dataCountBefore}`));
-  }
+scrape().catch(err => {
+  log(chalk.bold.bgRed.white('--- SOMETHING WENT WRONG ---'));
+  log(chalk.bold.bgRed.white('ATTEMPTING TO SAVE INITIALLY SCRAPED DATA'));
+  log('');
+  log(chalk.bold.bgRed.white('Report this error to the developer:'));
+  log(chalk.bold.bgRed.white(err));
+  log(chalk.bold.bgRed.white('--------------------------------------------'));  
+  
+  writeToFile();
 
   process.exit(0);
 });
+
